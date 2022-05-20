be1bd197e0f2fdde6d7b11e8498bd92e923fa61f
Calebens <rcalebe655@gmail.com>
2022-05-19 21:27:41 -0300

    atividades

--- atv10.cpp
+++ atv10.cpp
@@ -23,6 +23,15 @@ int main()
   v[1] = m[0][1] + m[1][1] + m[2][1];
   v[2] = m[0][2] + m[1][2] + m[2][2];
 
+  for (int i = 0; i < 3; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      cout<< m[i][j] << " ";
+    }
+    cout<< "\n";
+  }
+
   for (int j = 0; j < 3; j++)
   {
     cout << v[j] << endl;
