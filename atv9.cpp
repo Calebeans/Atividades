b4174654dc5af57cecec0ea26af216471653b83c
Calebens <rcalebe655@gmail.com>
2022-05-23 19:21:27 -0300

    atividades

--- atv9.cpp
+++ atv9.cpp
@@ -1,20 +1,11 @@
-#include <stdio.h>
 #include <iostream>
+ 
 using namespace std;
-
-int main()
-{
-    int s = -127;
-
-    cout << " Carac   Dec   Hex\n\n";
-    for (char i = -127; i < 127; i++){
-        cout << " " << i << "    "<< s << "     ";
-        printf("%X", s);
-        cout << "\n";
-        s++;
+ 
+int main() {
+    char m;
+    int i;
+    for(i=0;i<=127;i++){
+        printf("%d = %c = %X\n",i,i,i);
     }
-    cout << "\n     Carac   Dec     Hex\n\n";
-    cout << "   \x122 ENTER \x122   10      A\n\n";
-    system("PAUSE");
-    return 0;
 }
